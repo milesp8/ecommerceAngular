5a74b8d2f8db6e5cf90bd27fb4a9c19ce72a1785
Miles <milesphi@udel.edu>
2020-11-24 20:48:46 -0500

    Added homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogComponent
+    CatalogComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
