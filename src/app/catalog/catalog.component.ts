7e542398936be14f9e533ce7af4c1e31663c11b6
EddieLevin <edlevin@udel.edu>
2020-11-25 12:30:13 -0500

    catolog items are now added dynamically

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent implements OnInit {
-
-  constructor() { }
+export class CatalogComponent implements OnInit, AfterViewInit {
+  @ViewChild('grid') grid;
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit(): void{
+    let catalogHtml = '';
+    for(let i = 0; i < 20; i++){
+      catalogHtml += '<div class="grid-item"><a href="/home"><h3>Product ' + (i + 1) + '</h3><img class=images src=assets/img/new.png alt=Product Image width=200 height=200></a></div>';
+    }
+    this.renderer.setProperty(this.grid.nativeElement, 'innerHTML', catalogHtml);
   }
 
 }
