a76b068e6ec681c5edcb7817695d7d362c2abd0b
Miles <milesphi@udel.edu>
2020-11-24 21:04:43 -0500

    Redirects automatically to home page and added framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CatalogComponent} from 'src/app/catalog/catalog.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'catalog', component: CatalogComponent}
+  {path: 'catalog', component: CatalogComponent},
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home', component: HomeComponent}
 ];
 
 @NgModule({
