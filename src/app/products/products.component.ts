bfafc682e539fab85071a623c4b45032c9bf95b2
EddieLevin <edlevin@udel.edu>
2020-11-27 17:09:35 -0500

    id can now be retrieved in product page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -7,14 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  itemArr: { name: string, price: number, img: string, link: string }[] = [];
-  constructor() {
+  itemArr: { name: string, price: number, img: string, link: string, id: string }[] = [];
+  constructor(private router: Router) {
     const item = {
       name: 'Product 1',
       price: 29.99,
       img: 'assets/img/new.png',
       link: '/home',
-      text: "Trial text and product description, maybe with reviews?"
+      text:  "Trial text and product description, maybe with reviews?",
+      id: this.router.url.substr(10, this.router.url.length)
     };
     // tslint:disable-next-line: no-unused-expression
     this.itemArr.push(item);
