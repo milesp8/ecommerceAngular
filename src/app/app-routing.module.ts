e8a24ed87b01f8dd1ac6ccc11e38ce7d86b74589
EddieLevin <edlevin@udel.edu>
2020-11-27 14:22:19 -0500

    added cart and account components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import {CatalogComponent} from './catalog/catalog.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
   {path: 'catalog', component: CatalogComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'account', component: AccountComponent},
   {path: 'footer', component: FooterComponent},
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home', component: HomeComponent}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    CartComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
