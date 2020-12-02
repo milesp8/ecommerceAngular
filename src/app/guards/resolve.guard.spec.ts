e97ee5d416541c51a26413ed13df117c3a1964fe
youlkar <oyash.101@gmail.com>
2020-12-02 13:54:08 -0500

    ResolveGuard fix

--- src/app/guards/resolve.guard.spec.ts
+++ src/app/guards/resolve.guard.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ResolveGuard } from './resolve.guard';
+import { ProductsGuard, CategoriesGuard } from './resolve.guard';
 
 describe('ResolveGuard', () => {
-  let guard: ResolveGuard;
+  let guard: ProductsGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    guard = TestBed.inject(ResolveGuard);
+    guard = TestBed.inject(ProductsGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
+
+describe('CategoriesGuard', () => {
+  let guard: CategoriesGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    guard = TestBed.inject(CategoriesGuard);
   });
 
   it('should be created', () => {
