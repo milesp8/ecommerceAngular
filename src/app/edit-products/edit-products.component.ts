92bfa80c7cc5cd8bd783f7cd4be2126ed4549a6b
Miles <milesphi@udel.edu>
2020-12-04 14:00:46 -0500

    Edit categories of a product framework added

--- src/app/edit-products/edit-products.component.ts
+++ src/app/edit-products/edit-products.component.ts
@@ -12,4 +12,21 @@ export class EditProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleCollapse(){
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+    } 
+  }
+
 }
