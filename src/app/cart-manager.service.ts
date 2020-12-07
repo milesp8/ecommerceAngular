8e8b98cdb25ead528117caf1a89bb20d88210521
EddieLevin <edlevin@udel.edu>
2020-12-07 15:21:11 -0500

    added functionallity to remove button

--- src/app/cart-manager.service.ts
+++ src/app/cart-manager.service.ts
@@ -18,6 +18,13 @@ export class CartManagerService {
     return this.items;
   }
 
+  removeItem(index: number): {name: string, price: number, img: string, link: string, description: string} {
+    const removedItem = this.items[index];
+    this.items.splice(index, 1);
+    this.total -= removedItem.price;
+    return removedItem;
+  }
+
   clearCart(): {name: string, price: number, img: string, link: string, description: string}[] {
     this.items = [];
     this.total = 0;
