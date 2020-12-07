cfef410e590f70fd2ae1a6df3da28c1742c5e6e1
vincentb <vincentb@udel.edu>
2020-12-07 14:49:28 -0500

    Added required fields

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -16,28 +16,74 @@ export class PaymentComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  processCreditCard(): boolean {return true;}
+  processCreditCard(): boolean { return true; }
 
   addThisOrder(): void {
-    if (this.processCreditCard()) {
-    let name =
-      (document.getElementById("firstname") as HTMLInputElement).value
-      + " "
-      + (document.getElementById("lastname") as HTMLInputElement).value;
-    let email = (document.getElementById("email") as HTMLInputElement).value;
-    let address =
-      (document.getElementById("address1") as HTMLInputElement).value
-      + "\n"
-      + (document.getElementById("address2") as HTMLInputElement).value
-      + "\n"
-      + (document.getElementById("address3") as HTMLInputElement).value
-      + "\n"
-      + (document.getElementById("address4") as HTMLInputElement).value;
-    let total = this.cartManager.getTotal();
-    let items = this.cartManager.getItems();
-    let deliverydate = 0;
-    let order = {name: name, email: email, address: address, total: total, items: items, deliverydate: deliverydate}
-    this.appService.getAllProducts().forEach(a => console.log(a));
-    this.appService.addOrder(order);
-  }}
+    let canContinue = true;
+    if ((document.getElementById("firstname") as HTMLInputElement).value == "") {
+      canContinue = false;
+      document.getElementById("n1").innerText = " required*";
+    } else {
+      document.getElementById("n1").innerText = " *";
+    }
+    if ((document.getElementById("lastname") as HTMLInputElement).value == "") {
+      canContinue = false;
+      document.getElementById("n2").innerText = " required*";
+    } else {
+      document.getElementById("n2").innerText = " *";
+    }
+    if ((document.getElementById("email") as HTMLInputElement).value == "") {
+      canContinue = false;
+      document.getElementById("e").innerText = " required*";
+    } else {
+      document.getElementById("e").innerText = " *";
+    }
+    if ((document.getElementById("address1") as HTMLInputElement).value == "") {
+      canContinue = false;
+      document.getElementById("a1").innerText = " required*";
+    } else {
+      document.getElementById("a1").innerText = " *";
+    }
+    if ((document.getElementById("address2") as HTMLInputElement).value == "") {
+      canContinue = false;
+      document.getElementById("a2").innerText = " required*";
+    } else {
+      document.getElementById("a2").innerText = " *";
+    }
+    if ((document.getElementById("creditnumber") as HTMLInputElement).value == "") {
+      canContinue = false;
+      document.getElementById("c1").innerText = " required*";
+    } else {
+      document.getElementById("c1").innerText = " *";
+    }
+    if ((document.getElementById("creditcode") as HTMLInputElement).value == "") {
+      canContinue = false;
+      document.getElementById("c2").innerText = " required*";
+    } else {
+      document.getElementById("c2").innerText = " *";
+    }
+    if (canContinue) {
+      if (this.processCreditCard()) {
+        let name =
+          (document.getElementById("firstname") as HTMLInputElement).value
+          + " "
+          + (document.getElementById("lastname") as HTMLInputElement).value;
+        let email = (document.getElementById("email") as HTMLInputElement).value;
+        let address =
+          (document.getElementById("address1") as HTMLInputElement).value
+          + "\n"
+          + (document.getElementById("address2") as HTMLInputElement).value
+          + "\n"
+          + (document.getElementById("address3") as HTMLInputElement).value
+          + "\n"
+          + (document.getElementById("address4") as HTMLInputElement).value;
+        let total = this.cartManager.getTotal();
+        let items = this.cartManager.getItems();
+        let deliverydate = 0;
+        let order = { name: name, email: email, address: address, total: total, items: items, deliverydate: deliverydate }
+        this.appService.getAllProducts().forEach(a => console.log(a));
+        this.appService.addOrder(order);
+      }
+    }
+  }
 }
