07b52afb1662bccde74da044d0b51a74d8e0b56e
EddieLevin <edlevin@udel.edu>
2020-12-08 15:44:47 -0500

    disabled cookies

--- src/app/cart-manager.service.ts
+++ src/app/cart-manager.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+//import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +8,16 @@ export class CartManagerService implements OnInit{
   items: {name: string, price: number, img: string, link: string, description: string} [] = [];
   total = 0;
 
-  constructor(private cookie: CookieService){
-    if (this.cookie.get('cart') !== ''){this.items = JSON.parse(this.cookie.get('cart')); }
+  constructor(){
+    //if (this.cookie.get('cart') !== ''){this.items = JSON.parse(this.cookie.get('cart')); }
     if (this.items.length === 0){this.total = 0; }
-    this.total = parseInt(this.cookie.get('total'), 0);
+    //this.total = parseInt(this.cookie.get('total'), 0);
     if (isNaN(this.total)){this.total = 0; }
   }
   // tslint:disable-next-line: contextual-lifecycle
   ngOnInit(): void {
-    if (this.cookie.get('cart') !== ''){this.items = JSON.parse(this.cookie.get('cart')); }
-    this.total = parseInt(this.cookie.get('total'), 0);
+    //if (this.cookie.get('cart') !== ''){this.items = JSON.parse(this.cookie.get('cart')); }
+    //this.total = parseInt(this.cookie.get('total'), 0);
     if (this.items.length === 0){this.total = 0; }
     if (isNaN(this.total)){this.total = 0; }
   }
@@ -26,8 +26,8 @@ export class CartManagerService implements OnInit{
     if (!isNaN(product.price)) {
       this.items.push(product);
       this.total += Math.floor(product.price * 100);
-      this.cookie.set('cart', JSON.stringify(this.getItems()));
-      this.cookie.set('total', this.total.toString());
+      //this.cookie.set('cart', JSON.stringify(this.getItems()));
+      //this.cookie.set('total', this.total.toString());
     }
   }
 
@@ -40,16 +40,16 @@ export class CartManagerService implements OnInit{
     this.items.splice(index, 1);
     if (!isNaN(removedItem.price)) {this.total -= removedItem.price * 100; }
     if (this.items.length === 0){this.total = 0; }
-    this.cookie.set('cart', JSON.stringify(this.getItems()));
-    this.cookie.set('total', this.total.toString());
+    //this.cookie.set('cart', JSON.stringify(this.getItems()));
+    //this.cookie.set('total', this.total.toString());
     return removedItem;
   }
 
   clearCart(): {name: string, price: number, img: string, link: string, description: string}[] {
     this.items = [];
     this.total = 0;
-    this.cookie.set('cart', JSON.stringify(this.getItems()));
-    this.cookie.set('total', this.total.toString());
+    //this.cookie.set('cart', JSON.stringify(this.getItems()));
+    //this.cookie.set('total', this.total.toString());
     return this.items;
   }
 
