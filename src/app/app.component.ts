dd8d1b341611deb2539ac38ee2c3e19a2424b129
EddieLevin <edlevin@udel.edu>
2020-12-08 13:21:49 -0500

    opening and closing cart and menu is now animated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { AppServiceService } from './app-service.service';
-//import { AuthGuard } from './guards/auth.guard'
-import { AuthService } from './auth.service'
+// import { AuthGuard } from './guards/auth.guard'
+import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { waitForAsync } from '@angular/core/testing';
 
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('openClose', [
+    state('open', style({height: '100%', display: 'block'})),
+    state('close', style({height: '0%'})),
+    transition('open=>close', [animate('.25s')]),
+    transition('close=>open', [animate('.25s')]),
+    ])
+  ]
 })
+
 export class AppComponent implements OnInit {
   title = 'ecommerceAngular';
+  isOpenMenu = false;
+  isOpenCart = false;
 
-  constructor (private service : AppServiceService, private authservice: AuthService, private router: Router) {
+  constructor(private service: AppServiceService, private authservice: AuthService, private router: Router) {
 
   }
 
@@ -50,26 +63,35 @@ export class AppComponent implements OnInit {
 
   toggleDashboard() {
     if (this.authservice.loggedIn()) {
-      this.router.navigate(['/account'])
+      this.router.navigate(['/account']);
     } else {
-      this.router.navigate(['/login'])
+      this.router.navigate(['/login']);
     }
   }
 
   toggleMenu() {
-    var m = document.getElementById('menu');
+
+    let m = document.getElementById('menu');
+    /*
     if (m.style.display === "none") {
       m.style.display = "block";
     } else {
       m.style.display = "none";
     }
+    */
+    this.isOpenMenu = !this.isOpenMenu;
+    m.style.display = 'block';
   }
   toggleCart() {
-    var m = document.getElementById('cart');
-    if (m.style.display === "none") {
-      m.style.display = "block";
+    const m = document.getElementById('cart');
+    if (m.style.display === 'none') {
+      this.isOpenCart = !this.isOpenCart;
     } else {
-      m.style.display = "none";
+      m.style.display = 'none';
+      this.isOpenCart = !this.isOpenCart;
+
     }
+    m.style.display = 'block';
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,11 +41,12 @@ import { PaysuccessComponent } from './paysuccess/paysuccess.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule
   ],
   providers: [
-    AppServiceService, 
+    AppServiceService,
     ProductsGuard,
     CategoriesGuard,
     AuthGuard
