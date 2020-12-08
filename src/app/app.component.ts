4e205736b658336fa0f216d8a8e389bc8b5433b8
EddieLevin <edlevin@udel.edu>
2020-12-08 18:49:21 -0500

    fixed mistake when removing menu aimation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ import { waitForAsync } from '@angular/core/testing';
 
 export class AppComponent implements OnInit {
   title = 'ecommerceAngular';
-  isOpenMenu = false;
+  isOpenMenu = true;
   isOpenCart = false;
 
   constructor(private service: AppServiceService, private authservice: AuthService, private router: Router) {
